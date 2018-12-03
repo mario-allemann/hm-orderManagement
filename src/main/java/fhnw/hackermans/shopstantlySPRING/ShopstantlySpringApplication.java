4c539037e4d4952c5dd92e55356d724ef00575b1
mario-allemann <allemann2@hotmail.com>
2018-12-03 10:36:11 +0100

    where does this push to

---
+++ src/main/java/fhnw/hackermans/shopstantlySPRING/ShopstantlySpringApplication.java
@@ -0,0 +1,12 @@
+package fhnw.hackermans.shopstantlySPRING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopstantlySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopstantlySpringApplication.class, args);
+	}
+}
---
+++ src/test/java/fhnw/hackermans/shopstantlySPRING/ShopstantlySpringApplicationTests.java
@@ -0,0 +1,16 @@
+package fhnw.hackermans.shopstantlySPRING;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopstantlySpringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
