7b999195f8811afe4d90d5a9d828d579708a6706
phkaeppeli <32125119+phkaeppeli@users.noreply.github.com>
2018-12-12 14:41:46 +0100

    Changed db to mysql. Call /shipping to see the table and /addShipping?orderId=[value] to test

--- src/main/java/fhnw/hackermans/shopstantlySPRING/ShopstantlySpringApplication.java
+++ src/main/java/fhnw/hackermans/shopstantlySPRING/ShopstantlySpringApplication.java
@@ -1,15 +1,27 @@
 package fhnw.hackermans.shopstantlySPRING;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import fhnw.hackermans.shopstantlySPRING.domain.shipping.Shipping;
+import fhnw.hackermans.shopstantlySPRING.domain.shipping.ShippingRepo;
 
 @SpringBootApplication
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class ShopstantlySpringApplication {
+	@Autowired 
+	private TestRepo testRepo;
+	@Autowired 
+	private ShippingRepo shippingRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopstantlySpringApplication.class, args);
@@ -17,13 +29,40 @@ public class ShopstantlySpringApplication {
 
 	// Map homepage
 	@RequestMapping("/")
+	@ResponseBody
 	String home() {
 		return "This is the homepage, faggots";
 	}
 
 	// Map test page
 	@RequestMapping("/test")
-	String test() {
-		return "Test test test";
+	@ResponseBody
+	Iterable<TestTable> getAllTests() {
+		return testRepo.findAll();
+	}
+
+	@RequestMapping("/add")
+	public @ResponseBody String addTest(@RequestParam String testAtt) {
+		TestTable t = new TestTable();
+		t.setTestAtt(testAtt);
+		testRepo.save(t);
+		return "Saved";
+	}
+
+	// Map test page
+	@RequestMapping("/shipping")
+	@ResponseBody
+	Iterable<Shipping> getAllShippings() {
+		return shippingRepo.findAll();
+	}
+
+	@RequestMapping("/addShipping")
+	public @ResponseBody String addShipping(@RequestParam String orderId) {
+		Shipping s = new Shipping();
+		s.setEstimatedDate(new Date());
+		s.setEffectiveDate(new Date());
+		s.setOrderId(Integer.parseInt(orderId));
+		shippingRepo.save(s);
+		return "Saved";
 	}
 }
