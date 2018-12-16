66deac5da34ef7f792e674441e145608920a7be6
phkaeppeli <32125119+phkaeppeli@users.noreply.github.com>
2018-12-16 16:49:29 +0100

    test

--- src/main/java/fhnw/hackermans/shopstantlySPRING/domain/orderManagement/CustomerRepo.java
+++ src/main/java/fhnw/hackermans/shopstantlySPRING/domain/orderManagement/CustomerRepo.java
@@ -1,10 +1,12 @@
 package fhnw.hackermans.shopstantlySPRING.domain.orderManagement;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface CustomerRepo extends CrudRepository<Customer, Integer> {
-	Customer findByCustomerId(int customerId);
+	//@Query("SELECT * FROM Todo t where t.id = :id") 
+	Customer findByCustomerId(@Param("customerId") int customerId);
 }
