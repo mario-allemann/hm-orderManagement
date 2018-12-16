3af45e92a84f8014c0b1777a1c92ecb6bb6c99e3
phkaeppeli <32125119+phkaeppeli@users.noreply.github.com>
2018-12-16 17:06:59 +0100

    reverted

--- src/main/java/fhnw/hackermans/shopstantlySPRING/domain/orderManagement/OrderRepo.java
+++ src/main/java/fhnw/hackermans/shopstantlySPRING/domain/orderManagement/OrderRepo.java
@@ -1,12 +1,10 @@
 package fhnw.hackermans.shopstantlySPRING.domain.orderManagement;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface OrderRepo extends CrudRepository<Order, Integer> {
-	@Query("SELECT orderId, state, date FROM order")
-	Iterable<Order> findAll(Iterable<Integer> orderId);
+	
 }
