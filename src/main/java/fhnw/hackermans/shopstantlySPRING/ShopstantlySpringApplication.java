1203da724bf2be35b893ac56ff281bb950055eef
phkaeppeli <32125119+phkaeppeli@users.noreply.github.com>
2018-12-16 15:42:48 +0100

    test

--- src/main/java/fhnw/hackermans/shopstantlySPRING/ShopstantlySpringApplication.java
+++ src/main/java/fhnw/hackermans/shopstantlySPRING/ShopstantlySpringApplication.java
@@ -52,7 +52,7 @@ public class ShopstantlySpringApplication {
 		} catch (Exception e) {
 			// do nothing
 		}
-		Map<String, Object> outputContexts = (Map<String, Object>)jsonMap.get("queryResults");
+		Map<String, Object> outputContexts = (Map<String, Object>)jsonMap.get("queryResult");
 		String retString = "";
 		for (Map.Entry<String, Object> entry : outputContexts.entrySet()) {
 			retString += "KEY: " + entry.getKey() + "-> VALUE: " + entry.getValue().toString() + "\r\n";
